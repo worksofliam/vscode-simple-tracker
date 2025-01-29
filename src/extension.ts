17c70bfc7565c3af18be005f0be44881c7a0a5ab
worksofliam <mrliamallan@live.co.uk>
2025-01-29 16:55:01 -0500

    Working tracker
    Signed-off-by: worksofliam <mrliamallan@live.co.uk>

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,14 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import { TimeManager } from './manager';
+import { TimeManager, TrackerDetails } from './manager';
 import { getGitBranch } from './git';
+import path from 'path';
 
 let tracker: TimeManager;
+let updateInterval: NodeJS.Timeout;
+
+const HALF_MINUTE = 30 * 1000;
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -15,18 +19,36 @@ export async function activate(context: vscode.ExtensionContext) {
 	await tracker.load();
 
 	const statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 0);
-	statusBarItem.text = '$(clock) 0';
-	statusBarItem.show();
 
-	tracker.changeEvent = (projects) => {
-		statusBarItem.text = `$(clock) ${Object.keys(projects).length}`;
+	const updateStats = () => {
+		const md = getStatsMd();
+		statusBarItem.tooltip = md;
 	};
 
-	vscode.workspace.workspaceFolders?.forEach((folder) => {
-		tracker.startTracking(folder);
-	});
+	if (vscode.workspace.workspaceFolders) {
+		statusBarItem.text = `$(clock) ${vscode.workspace.workspaceFolders.length}`;
+		statusBarItem.show();
+
+		updateInterval = setInterval(() => {
+			statusBarItem.text = `$(clock) ${tracker.getTracking().length}`;
+			tracker.updateAllDaySeconds();
+
+			updateStats();
+	
+			// Let's do a backup save!
+			tracker.save();
+		}, HALF_MINUTE);
+
+		vscode.workspace.workspaceFolders.forEach((folder) => {
+			tracker.startTracking(folder);
+		});
+	}
 
 	context.subscriptions.push(
+		{dispose: () => {
+			clearInterval(updateInterval);
+		}},
+		statusBarItem,
 		vscode.workspace.onDidSaveTextDocument((e) => {
 			const ws = vscode.workspace.getWorkspaceFolder(e.uri);
 			if (ws) {
@@ -35,6 +57,8 @@ export async function activate(context: vscode.ExtensionContext) {
 					tracker.addBranch(ws, branch);
 				}
 			}
+
+			tracker.addSave(path.extname(e.fileName));
 		}),
 		vscode.workspace.onDidChangeWorkspaceFolders((e) => {
 			e.added.forEach((folder) => {
@@ -48,10 +72,57 @@ export async function activate(context: vscode.ExtensionContext) {
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() {
+export async function deactivate() {
 	if (tracker) {
-		vscode.workspace.workspaceFolders?.forEach((folder) => {
-			tracker.endTracking(folder);
-		});
+		// It's too risky having the save it. It half writes and then the process cuts it off.
+
+		// vscode.workspace.workspaceFolders?.forEach((folder) => {
+		// 	tracker.endTracking(folder);
+		// });
+
+		// await tracker.save();
 	}
+}
+
+function getStatsMd() {
+	const projectIds = vscode.workspace.workspaceFolders ? vscode.workspace.workspaceFolders.map((f) => f.name) : [];
+
+	let markdown: string[] = [];
+
+	projectIds.forEach((id, i) => {
+		const today = tracker.getStatsForPeriod(id, 1);
+		const week = tracker.getStatsForPeriod(id, 7);
+
+		markdown.push(
+			`### ${id}`,
+			``,
+			`#### Today`,
+			``,
+			`* $(history) ${formatSeconds(today.seconds)}`,
+			...(today.branches!.length ? [`* $(git-branch) worked on ${today.branches!.join(", ")}`] : []),
+			``,
+			`#### This Week`,
+			``,
+			`* $(history) ${formatSeconds(week.seconds)}`,
+			...(week.branches!.length ? [`* $(git-branch) worked on ${week.branches!.join(", ")}`] : [])
+		);
+
+		if (i < projectIds.length - 1) {
+			markdown.push(
+				``,
+				`---`,
+				``
+			);
+		}
+	});
+
+	const md = new vscode.MarkdownString(markdown.join("\n"));
+	md.supportThemeIcons = true;
+	return md;
+}
+
+function formatSeconds(seconds: number) {
+	const hours = Math.floor(seconds / 3600);
+	const minutes = Math.floor((seconds % 3600) / 60);
+	return `${hours}h ${minutes}m`;
 }
--- src/manager.ts
+++ src/manager.ts
@@ -3,122 +3,232 @@ import path from "path";
 import { env } from "process";
 import { WorkspaceFolder } from "vscode";
 
-interface OptionalTrackerDetails {
-  branches?: string[];
-}
-
-interface TrackerDetails extends OptionalTrackerDetails {
+export interface TrackerDetails {
+  branches: string[];
   seconds: number;
 }
 
-interface TrackerFile {
-  [date: string]: {
-    projects: {
-      [project: string]: TrackerDetails;
-    }
+interface DayData {
+  projects: {
+    [project: string]: TrackerDetails;
+  }
+  saves: {
+    [ext: string]: number
   }
 }
 
-export type ActiveProjectList = {[id: string]: Date};
-const TRACKER_FILE = path.join(env.HOME || ".", ".vscode-tracker.json");
+type SchemaVersion = "v1";
+const CURRENT_SCHEMA_VERSION: SchemaVersion = `v1`;
+interface TrackerFile {
+  version: SchemaVersion;
+  days: {[date: string]: DayData}
+}
+
+export type ActiveProjectList = { [id: string]: Date };
+
+const TRACKER_FILE = path.join(env.HOME || ".", `.vscode-tracker-${new Date().getFullYear()}.json`);
+
+function secondsSize(pastTime: Date) {
+  return (new Date().getTime() - pastTime.getTime()) / 1000;
+}
+
+function dateString(minusDays = 0) {
+  // get dd/mm/yyyy
+  const specificDate = new Date();
+  specificDate.setDate(specificDate.getDate() - minusDays);
+  return `${specificDate.getDate()}/${specificDate.getMonth()+1}/${specificDate.getFullYear()}`;
+}
 
 export class TimeManager {
-  private store: TrackerFile = {};
+  private store: TrackerFile = {version: "v1", days: {}};
   private activeProjects: ActiveProjectList = {};
 
-  changeEvent?: (projects: ActiveProjectList) => void;
+	getTracking() {
+		return Object.keys(this.activeProjects);
+	}
 
   async load() {
     // load from file
-    
+
     try {
       const contents = await readFile(TRACKER_FILE, "utf-8");
       this.store = JSON.parse(contents);
+
+      if (this.store.version !== CURRENT_SCHEMA_VERSION) {
+        for (const day in this.store) {
+          this.validateDaySchema(day, {all: true});
+        }
+      }
     } catch (error) {
       console.error("Failed to load tracker file", error);
     }
   }
 
-  private save() {
+  public save() {
     // save to file
     const contents = JSON.stringify(this.store, null, 2);
     return writeFile(TRACKER_FILE, contents, "utf-8");
   }
 
-  private trigger() {
-    if (this.changeEvent) {
-      this.changeEvent(this.activeProjects);
+  /**
+   * Returns time and branches worked on for a specific branch over a certain amount of days
+   */
+  public getStatsForPeriod(project: string, days: number): TrackerDetails {
+    const dayKeys = this.getDays(days);
+    const stats: TrackerDetails = { seconds: 0, branches: [] };
+
+    for (const day of dayKeys) {
+      const dayStats = this.getStats(day);
+      if (dayStats && dayStats.projects[project]) {
+        stats.seconds += dayStats.projects[project].seconds;
+
+        if (dayStats.projects[project].branches) {
+          stats.branches = [...new Set([...(stats.branches!), ...dayStats.projects[project].branches])];
+        }
+      }
+    }
+
+    return stats;
+  }
+
+  /**
+   * Gets data for a specific day
+   */
+  public getStats(day: string): DayData|undefined {
+    // TODO: honestly is there a better way to deep clone?
+    if (this.store.days[day]) {
+      const storedStats: DayData = JSON.parse(JSON.stringify(this.store.days[day]));
+
+      if (day === dateString()) {
+        for (const wsName in storedStats.projects) {
+          const activeProjectStarted = this.activeProjects[wsName];
+          if (activeProjectStarted) {
+            const secondsSinceThen = secondsSize(activeProjectStarted);
+            storedStats.projects[wsName].seconds += secondsSinceThen;
+          }
+        }
+      }
+
+      return storedStats;
     }
   }
 
-  private dateString() {
-    // get dd/mm/yyyy
-    const now = new Date();
-    return `${now.getDate()}/${now.getMonth()}/${now.getFullYear()}`;
+  /**
+   * Gets days where data is available
+   */
+  public getDays(lastAmount?: number) {
+    if (lastAmount) {
+      let dayKeys = [];
+      for (let i = 0; i < lastAmount; i++) {
+        let key = dateString(i);
+        dayKeys.push(key);
+      }
+      return dayKeys;
+
+    } else {
+      return Object.keys(this.store).reverse();
+    }
   }
 
   startTracking(ws: WorkspaceFolder) {
     this.activeProjects[ws.name] = new Date();
-
-    this.trigger();
   }
-
-  private getProjectDetails(ws: WorkspaceFolder): OptionalTrackerDetails {
-    const today = this.dateString();
-    
-    if (this.store[today] && this.store[today].projects[ws.name]) {
-      return this.store[today].projects[ws.name];
+  
+  private validateDaySchema(chosenDay: string, project: { id?: string, all?: boolean } = {}) {
+    if (!this.store.days[chosenDay]) {
+      this.store.days[chosenDay] = { projects: {}, saves: {} };
     }
 
-    return {};
-  }
-
-  private updateDetails(ws: WorkspaceFolder, details: OptionalTrackerDetails) {
-    const today = this.dateString();
-    if (!this.store[today]) {
-      this.store[today] = { projects: {} };
+    if (!this.store.days[chosenDay].projects) {
+      this.store.days[chosenDay].projects = {};
     }
 
-    const existingData = this.store[today].projects[ws.name] || { seconds: 0 };
+    if (!this.store.days[chosenDay].saves) {
+      this.store.days[chosenDay].saves = {};
+    }
 
-    this.store[today].projects[ws.name] = {
-      ...existingData,
-      ...details
+    const fixProject = (id: string) => {
+      if (this.store.days[chosenDay].projects[id]) {
+        if (!this.store.days[chosenDay].projects[id].branches) {
+          this.store.days[chosenDay].projects[id].branches = [];
+        }
+
+        if (!this.store.days[chosenDay].projects[id].seconds) {
+          this.store.days[chosenDay].projects[id].seconds = 0;
+        }
+
+      } else {
+        this.store.days[chosenDay].projects[id] = { seconds: 0, branches: [] };
+      }
     };
+
+    if (project.id) {
+      fixProject(project.id);
+    } else if (project.all) {
+      for (const id in this.store.days[chosenDay].projects) {
+        fixProject(id);
+      }
+    }
+  }
+
+  private updateDetails(projectId: string, details: Partial<TrackerDetails>) {
+    const ADD_PROPS = [`seconds`];
+    const today = dateString();
+    
+    this.validateDaySchema(today, {id: projectId});
+
+    const existingData: any = this.store.days[today].projects[projectId];
+
+    for (const key in details) {
+      if (Array.isArray((details as any)[key])) {
+        // Merge
+        existingData[key] = [...new Set([...(existingData[key] || []), ...(details as any)[key]])];
+      } else {
+        if (ADD_PROPS.includes(key)) {
+          existingData[key] += (details as any)[key];
+        } else {
+          existingData[key] = (details as any)[key];
+        }
+      }
+    }
   }
 
   addBranch(ws: WorkspaceFolder, branch: string) {
-    const details = this.getProjectDetails(ws);
-    if (!details.branches) {
-      details.branches = [];
+    this.updateDetails(ws.name, { branches: [branch] });
+  }
+
+  addSave(ext: string) {
+    const today = dateString();
+    
+    this.validateDaySchema(today);
+
+    if (!this.store.days[today].saves[ext]) {
+      this.store.days[today].saves[ext] = 0;
     }
 
-    if (details.branches.includes(branch)) {return;}
+    this.store.days[today].saves[ext]++;
+  }
 
-    details.branches.push(branch);
-    this.updateDetails(ws, details);
+  public updateAllDaySeconds() {
+    for (const wsName in this.activeProjects) {
+      this.updateDaySeconds(wsName);
+    }
+  }
+
+  private updateDaySeconds(specificProject: string) {
+    const start = this.activeProjects[specificProject];
+    if (!start) { return; }
+
+    let seconds = Math.max(0, secondsSize(start));
+
+    this.updateDetails(specificProject, { seconds });
+
+    this.activeProjects[specificProject] = new Date();
   }
 
   endTracking(ws: WorkspaceFolder) {
-    const start = this.activeProjects[ws.name];
-    if (!start) {return;}
-
-    let seconds = Math.max(0, (new Date().getTime() - start.getTime()) / 1000);
-
-    const today = this.dateString();
-    if (!this.store[today]) {
-      this.store[today] = { projects: {} };
-    }
-
-    if (!this.store[today].projects[ws.name]) {
-      this.store[today].projects[ws.name] = { seconds: 0 };
-    }
-
-    this.store[today].projects[ws.name].seconds += seconds;
+    this.updateDaySeconds(ws.name);
 
     delete this.activeProjects[ws.name];
-    this.trigger();
-
-    this.save();
   }
 }
